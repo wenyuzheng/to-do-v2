5cc61d26bdb379b024c2363ec40e8a31ca52a15d
Wenyu Zheng <zhengwy229@gmail.com>
2023-05-30 11:31:20 +0100

    Toggle complete

--- src/features/todos/TodoItem.js
+++ src/features/todos/TodoItem.js
@@ -1,5 +1,21 @@
+import { useDispatch } from "react-redux";
+
+const TickBox = ({ id }) => {
+  const dispatch = useDispatch();
+  const onChangeHandler = () => {
+    dispatch({ type: "todos/todoToggled", payload: id });
+  };
+
+  return <input type="checkbox" onChange={onChangeHandler} />;
+};
+
 const TodoItem = ({ todo }) => {
-  return <div>{todo.text}</div>;
+  return (
+    <div style={{ display: "inline-flex" }}>
+      <TickBox id={todo.id} />
+      <div>{todo.text}</div>
+    </div>
+  );
 };
 
 export default TodoItem;
