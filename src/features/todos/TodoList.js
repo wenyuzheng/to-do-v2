e92c0df1133fdebbcc31bfd2570ed93142965a07
Wenyu Zheng <zhengwy229@gmail.com>
2023-07-11 18:31:57 +0100

    createSeletor colours

--- src/features/todos/TodoList.js
+++ src/features/todos/TodoList.js
@@ -1,7 +1,7 @@
 import { useSelector } from "react-redux";
 import TodoItem from "./TodoItem";
 import _ from "lodash";
-import { selectFilteredTodoIds } from "./todosReducer";
+import { selectFilteredTodoIds } from "./filteredTodosSelector";
 
 const TodoList = () => {
   const todoIds = useSelector(selectFilteredTodoIds);
--- src/features/todos/filteredTodosSelector.js
+++ src/features/todos/filteredTodosSelector.js
@@ -1,3 +1,5 @@
+import { createSelector } from "reselect";
+
 export const filterTodosShowing = (todos, showing) => {
   if (showing === "Completed") {
     return todos.filter((todo) => todo.complete);
@@ -13,8 +15,17 @@ export const filterTodosColour = (todos, colours) => {
   return todos.filter((todo) => colours.includes(todo.colour));
 };
 
-export default (todos, filters) => {
-  const filteredColour = filterTodosColour(todos, filters.colours);
-  const filtered = filterTodosShowing(filteredColour, filters.showing);
-  return filtered;
-};
+export const selectFilteredTodos = createSelector(
+  (state) => state.todos,
+  (state) => state.filters,
+  (todos, filters) => {
+    const filteredColour = filterTodosColour(todos, filters.colours);
+    const filtered = filterTodosShowing(filteredColour, filters.showing);
+    return filtered;
+  }
+);
+
+export const selectFilteredTodoIds = createSelector(
+  selectFilteredTodos,
+  (filteredTodos) => filteredTodos.map((e) => e.id)
+);
